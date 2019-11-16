4d4597b8785c2bcb3e53f192c022fd15b2ae76d9
keyvan F.A <keyvan.fa89@live.com>
2019-11-16 05:46:21 +0330

    Add ExcelHelper Classes

---
+++ K1.Helpers/ExcelHelper/ExcelReaderSetting.cs
@@ -0,0 +1,27 @@
+﻿namespace K1.Libs.Utils.ExcelHelper
+{
+    public class ExcelReaderSetting
+    {
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Default: , ; TAB | # 
+        /// </summary>
+        public char[] CsvSeparators { get; set; }
+
+        public bool? UseColumnDataType { get; set; }
+
+        /// <summary>
+        /// select sheets by its names to read
+        /// by defalut read all sheets
+        /// </summary>
+        public string[] SheetsNames { get; set; }
+
+        /// <summary>
+        /// select sheets by its index to read
+        /// by defalut read all sheets
+        /// </summary>
+        public int[] SheetsIndexes { get; set; }
+
+    }
+}
---
+++ K1.Helpers/ExcelHelper/IExcelHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.IO;
+
+namespace K1.Libs.Utils.ExcelHelper
+{
+    public interface IExcelHelper
+    {
+        DataSet Read(FileStream excelFile);
+        IEnumerable<TModel> Read<TModel>(FileStream excelFile);
+    }
+}
